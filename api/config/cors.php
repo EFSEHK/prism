80063e8cbfa4267a028cdc484b28c132c1c6b165
Shaad Ahmed <shaadahmed93@gmail.com>
2026-07-01 11:49:05 +0500

    tried resolving cors error

--- api/config/cors.php
+++ api/config/cors.php
@@ -1,12 +1,23 @@
 <?php
 
+$fromEnv = array_filter(array_map('trim', explode(',', (string) env(
+    'CORS_ALLOWED_ORIGINS',
+    env('LARAVEL_CORS_ALLOWED_ORIGINS', '')
+))));
+
+$defaultOrigins = array_filter([
+    'http://localhost:5173',
+    'http://localhost:8081',
+    env('PRODUCTION_WEB_URL', 'https://sap.innovisiq.com'),
+]);
+
 return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', env('LARAVEL_CORS_ALLOWED_ORIGINS', 'http://localhost:5173'))))),
+    'allowed_origins' => array_values(array_unique(array_merge($defaultOrigins, $fromEnv))),
 
     'allowed_origins_patterns' => [],
 
